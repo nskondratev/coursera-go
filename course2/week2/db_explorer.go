ae60884eb935514d2f783abc50ea3e9f447f9f6e
Nikita Kondratev <highflyer16@yandex.ru>
2019-04-26 19:23:23 +0300

    WIP. Start implementing GetRecordsList method

--- course2/week2/db_explorer.go
+++ course2/week2/db_explorer.go
@@ -3,9 +3,11 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"reflect"
 	"strconv"
 	"strings"
 )
@@ -54,13 +56,13 @@ func newPathParams(r *http.Request) (*pathParams, error) {
 	}
 	rl := r.URL.Query().Get("limit")
 
-	if il, err := strconv.Atoi(rl); err != nil {
+	if il, err := strconv.Atoi(rl); len(rl) > 0 && err != nil {
 		pp.Limit = il
 	}
 
 	ro := r.URL.Query().Get("offset")
 
-	if io, err := strconv.Atoi(ro); err != nil {
+	if io, err := strconv.Atoi(ro); len(ro) > 0 && err != nil {
 		pp.Offset = io
 	}
 
@@ -88,6 +90,10 @@ type listTablesResponse struct {
 	Tables []string `json:"tables"`
 }
 
+type listRecordsResponse struct {
+	Records []map[string]interface{} `json:"records"`
+}
+
 func writeJSON(w http.ResponseWriter, statusCode int, p interface{}) {
 	if statusCode < 0 {
 		statusCode = http.StatusInternalServerError
@@ -100,7 +106,13 @@ func writeJSON(w http.ResponseWriter, statusCode int, p interface{}) {
 }
 
 type dbExplorer struct {
-	db *sql.DB
+	db     *sql.DB
+	tables map[string]struct {
+		Columns []struct {
+			Name string
+			Type string
+		}
+	}
 }
 
 func NewDbExplorer(db *sql.DB) (*dbExplorer, error) {
@@ -117,6 +129,8 @@ func (de *dbExplorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch {
 	case pp.isListTables():
 		de.HandleGetTablesList(w, r)
+	case pp.isRecordsList():
+		de.HandleRecordsList(w, r, pp)
 	default:
 		writeJSON(w, http.StatusNotFound, &responseEnvelope{Error: "unknown path"})
 	}
@@ -147,6 +161,55 @@ func (de *dbExplorer) GetTablesList() ([]string, error) {
 	return res, nil
 }
 
+func (de *dbExplorer) GetRecordsList(table string, limit, offset int) ([]map[string]interface{}, error) {
+	res := make([]map[string]interface{}, 0)
+	currTables, err := de.GetTablesList()
+	if err != nil {
+		return res, &apiError{Err: err, HTTPStatus: http.StatusInternalServerError}
+	}
+	tableExists := false
+	for _, tn := range currTables {
+		if table == tn {
+			tableExists = true
+			break
+		}
+	}
+	if !tableExists {
+		return res, &apiError{Err: errors.New("unknown table"), HTTPStatus: http.StatusNotFound}
+	}
+	rows, err := de.db.Query("SELECT * FROM `"+table+"` LIMIT ? OFFSET ?", limit, offset)
+	if err != nil {
+		return res, &apiError{Err: err, HTTPStatus: http.StatusInternalServerError}
+	}
+	cols, err := rows.ColumnTypes()
+	if err != nil {
+		return res, &apiError{Err: err, HTTPStatus: http.StatusInternalServerError}
+	}
+	for rows.Next() {
+		values := make([]interface{}, len(cols))
+
+		for i, col := range cols {
+			values[i] = reflect.New(col.ScanType()).Interface()
+		}
+
+		err := rows.Scan(values...)
+		log.Printf("Scanned values: %#v", values)
+		if err != nil {
+			return res, &apiError{Err: err, HTTPStatus: http.StatusInternalServerError}
+		}
+		toAdd := make(map[string]interface{})
+
+		for i, col := range cols {
+			log.Printf("Process column %s, value: %#v", col.Name(), values[i])
+			toAdd[col.Name()] = values[i]
+		}
+
+		res = append(res, toAdd)
+	}
+
+	return res, nil
+}
+
 func (de *dbExplorer) HandleGetTablesList(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		writeJSON(w, http.StatusNotAcceptable, &responseEnvelope{Error: "bad method"})
@@ -162,4 +225,22 @@ func (de *dbExplorer) HandleGetTablesList(w http.ResponseWriter, r *http.Request
 		return
 	}
 	writeJSON(w, http.StatusOK, &responseEnvelope{Response: &listTablesResponse{Tables: res}})
+}
+
+func (de *dbExplorer) HandleRecordsList(w http.ResponseWriter, r *http.Request, pp *pathParams) {
+	switch r.Method {
+	case http.MethodGet:
+		res, err := de.GetRecordsList(*pp.Table, pp.Limit, pp.Offset)
+		if err != nil {
+			c := http.StatusInternalServerError
+			if ae, ok := err.(apiError); ok {
+				c = ae.HTTPStatus
+			}
+			writeJSON(w, c, &responseEnvelope{Error: err.Error()})
+			return
+		}
+		writeJSON(w, http.StatusOK, &responseEnvelope{Response: &listRecordsResponse{Records: res}})
+	default:
+		writeJSON(w, http.StatusNotAcceptable, &responseEnvelope{Error: "bad method"})
+	}
 }

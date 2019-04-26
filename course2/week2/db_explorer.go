68b8e613344d7033a32e5a9651fc851c6e85b0fc
Nikita Kondratev <highflyer16@yandex.ru>
2019-04-26 13:46:27 +0300

    Feature. Add pathParams struct and construct it from request

--- course2/week2/db_explorer.go
+++ course2/week2/db_explorer.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 // тут вы пишете код
@@ -18,6 +20,63 @@ type apiError struct {
 
 func (ae apiError) Error() string {
 	return fmt.Sprintf("api error %s", ae.Err.Error())
+}
+
+type pathParams struct {
+	Table  *string
+	ID     *int
+	Limit  int
+	Offset int
+}
+
+func (pp pathParams) String() string {
+	return fmt.Sprintf("pathParams{Table: %v, ID: %v, Limit: %d, Offset: %d}", pp.Table, pp.ID, pp.Limit, pp.Offset)
+}
+
+func newPathParams(r *http.Request) (*pathParams, error) {
+	pp := &pathParams{Limit: 5, Offset: 0}
+	p := strings.Split(r.URL.Path, "/")
+	log.Printf("Splitted path: %#v", p)
+	for i, pathPart := range p {
+		if len(pathPart) < 1 {
+			continue
+		}
+		switch i {
+		case 1:
+			pp.Table = &pathPart
+		case 2:
+			id, err := strconv.Atoi(pathPart)
+			if err != nil {
+				return pp, err
+			}
+			pp.ID = &id
+		}
+	}
+	rl := r.URL.Query().Get("limit")
+
+	if il, err := strconv.Atoi(rl); err != nil {
+		pp.Limit = il
+	}
+
+	ro := r.URL.Query().Get("offset")
+
+	if io, err := strconv.Atoi(ro); err != nil {
+		pp.Offset = io
+	}
+
+	return pp, nil
+}
+
+func (pp pathParams) isListTables() bool {
+	return pp.Table == nil && pp.ID == nil
+}
+
+func (pp pathParams) isRecordsList() bool {
+	return pp.Table != nil && pp.ID == nil
+}
+
+func (pp pathParams) isRecord() bool {
+	return pp.Table != nil && pp.ID != nil
 }
 
 type responseEnvelope struct {
@@ -50,8 +109,13 @@ func NewDbExplorer(db *sql.DB) (*dbExplorer, error) {
 
 func (de *dbExplorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("[%s] New request on path: %s", r.Method, r.URL.Path)
-	switch r.URL.Path {
-	case "/":
+	pp, err := newPathParams(r)
+	if err != nil {
+		writeJSON(w, http.StatusNotAcceptable, &responseEnvelope{Error: err.Error()})
+	}
+	log.Printf("parsed path params: %s", pp)
+	switch {
+	case pp.isListTables():
 		de.HandleGetTablesList(w, r)
 	default:
 		writeJSON(w, http.StatusNotFound, &responseEnvelope{Error: "unknown path"})

faa2af9a07d4d052af742c684a02a5d14c0abc7e
Nikita Kondratev <highflyer16@yandex.ru>
2019-04-28 13:28:16 +0300

    Feature. Implement ACL interceptors

--- course2/week3/service.go
+++ course2/week3/service.go
@@ -4,19 +4,59 @@ import (
 	"context"
 	"encoding/json"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"log"
-
 	"net"
+	"regexp"
 )
 
 // тут вы пишете код
 // обращаю ваше внимание - в этом задании запрещены глобальные переменные
 
-type aclData map[string][]string
+type aclData struct {
+	data map[string][]string
+	sr   *regexp.Regexp
+}
+
+func (a *aclData) checkACL(consumer, method string) bool {
+	allowedMethods, ok := a.data[consumer]
+
+	mn := "aclData.checkACL"
+	log.Printf("[%s] consumer: %s, method: %s", mn, consumer, method)
+
+	if !ok {
+		return false
+	}
+
+	parsedMethod := a.sr.Split(method, -1)
+
+	log.Printf("[%s] parsedMethod: %v", mn, parsedMethod)
+
+	if len(parsedMethod) != 3 {
+		log.Printf("[%s] parsedMethod len not 3, return false", mn)
+		return false
+	}
+
+	for _, m := range allowedMethods {
+		pm := a.sr.Split(m, -1)
+		log.Printf("[%s] Process: %v", mn, pm)
+		if pm[1] == parsedMethod[1] && (pm[2] == "*" || pm[2] == parsedMethod[2]) {
+			log.Printf("[%s] Return true", mn)
+			return true
+		}
+	}
+
+	return false
+}
 
 func newAclDataFromJSON(in string) (*aclData, error) {
-	res := &aclData{}
-	if err := json.Unmarshal([]byte(in), res); err != nil {
+	res := &aclData{
+		data: make(map[string][]string),
+		sr:   regexp.MustCompile(`/`),
+	}
+	if err := json.Unmarshal([]byte(in), &res.data); err != nil {
 		return nil, err
 	}
 	return res, nil
@@ -30,7 +70,7 @@ type MyService struct {
 
 type AdminService struct{}
 
-func NewAdminService() *AdminService {
+func NewAdminService() AdminServer {
 	return &AdminService{}
 }
 
@@ -42,26 +82,126 @@ func (as *AdminService) Statistics(in *StatInterval, s Admin_StatisticsServer) e
 	return nil
 }
 
+type BizService struct{}
+
+func NewBizService() BizServer {
+	return &BizService{}
+}
+
+func (bs *BizService) Check(ctx context.Context, in *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func (bs *BizService) Add(ctx context.Context, in *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func (bs *BizService) Test(ctx context.Context, in *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func (s *MyService) checkACL(consumer, method string) bool {
+	return s.aclData.checkACL(consumer, method)
+}
+
+func (s *MyService) getUnaryACLInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		log.Printf("aclInterceptor call")
+		md, ok := metadata.FromIncomingContext(ctx)
+
+		if !ok {
+			return nil, status.Errorf(codes.Unauthenticated, "unable to parse metadata from request context")
+		}
+
+		cons := md.Get("consumer")
+
+		log.Printf("[%s] consumers: %#v", "aclInterceptor", cons)
+
+		if len(cons) < 1 {
+			return nil, status.Errorf(codes.Unauthenticated, "consumer is not provided")
+		}
+
+		log.Printf("[%s] full method: %#v", "aclInterceptor", info.FullMethod)
+
+		if !s.checkACL(cons[0], info.FullMethod) {
+			return nil, status.Errorf(codes.Unauthenticated, "method not allowed")
+		}
+
+		return handler(ctx, req)
+	}
+}
+
+type wrappedStream struct {
+	grpc.ServerStream
+}
+
+func (w *wrappedStream) RecvMsg(m interface{}) error {
+	return w.ServerStream.RecvMsg(m)
+}
+
+func (w *wrappedStream) SendMsg(m interface{}) error {
+	return w.ServerStream.SendMsg(m)
+}
+
+func newWrappedStream(s grpc.ServerStream) grpc.ServerStream {
+	return &wrappedStream{s}
+}
+
+func (s *MyService) getStreamACLInterceptor() grpc.StreamServerInterceptor {
+	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		md, ok := metadata.FromIncomingContext(ss.Context())
+
+		if !ok {
+			return status.Errorf(codes.Unauthenticated, "unable to parse metadata from request context")
+		}
+
+		cons := md.Get("consumer")
+
+		log.Printf("[%s] consumers: %#v", "aclInterceptor", cons)
+
+		if len(cons) < 1 {
+			return status.Errorf(codes.Unauthenticated, "consumer is not provided")
+		}
+
+		log.Printf("[%s] full method: %#v", "aclInterceptor", info.FullMethod)
+
+		if !s.checkACL(cons[0], info.FullMethod) {
+			return status.Errorf(codes.Unauthenticated, "method not allowed")
+		}
+
+		return handler(srv, newWrappedStream(ss))
+	}
+}
+
 func (s *MyService) start() error {
 	log.Printf("Starting MyService at %s", s.listenAddr)
+
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(s.getUnaryACLInterceptor()),
+		grpc.StreamInterceptor(s.getStreamACLInterceptor()),
+	)
+	RegisterAdminServer(server, NewAdminService())
+	RegisterBizServer(server, NewBizService())
+
 	lis, err := net.Listen("tcp", s.listenAddr)
 	if err != nil {
 		return err
 	}
 
-	server := grpc.NewServer()
-	RegisterAdminServer(server, NewAdminService())
+	go func() {
+		if err := server.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
 
 	go func() {
 		select {
 		case <-s.ctx.Done():
 			server.Stop()
-			if err := lis.Close(); err != nil {
-				log.Printf("error while closing listener: %v", err)
-			}
 			return
 		}
 	}()
+
 	return nil
 }
 

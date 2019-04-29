05c814ec931312e08e268bcae885ce0703f8a372
Nikita Kondratev <highflyer16@yandex.ru>
2019-04-29 21:22:38 +0300

    WIP. Start implementing i2s func

--- course2/week4/i2s.go
+++ course2/week4/i2s.go
@@ -1,7 +1,30 @@
 package main
 
-/*
+import (
+	"errors"
+	"log"
+	"reflect"
+)
+
 func i2s(data interface{}, out interface{}) error {
-	// todo
+	if reflect.ValueOf(out).Kind() != reflect.Ptr {
+		return errors.New("return non pointer")
+	}
+
+	val := reflect.ValueOf(out).Elem()
+
+	log.Printf("Input: %#v", data)
+	log.Printf("Receive %#v", val)
+
+	switch val.Kind() {
+	case reflect.Slice:
+		log.Printf("Receive slice")
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			typeField := val.Type().Field(i)
+			log.Printf("Process field %#v", typeField)
+		}
+	}
+
+	return nil
 }
-*/

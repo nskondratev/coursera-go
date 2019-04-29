c1bd8aaf7afbaf08a40e3c9dbef99dbb484ca019
Nikita Kondratev <highflyer16@yandex.ru>
2019-04-29 22:57:17 +0300

    Feature. Implement i2s func

--- course2/week4/i2s.go
+++ course2/week4/i2s.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"errors"
-	"log"
+	"fmt"
 	"reflect"
 )
 
@@ -13,16 +13,59 @@ func i2s(data interface{}, out interface{}) error {
 
 	val := reflect.ValueOf(out).Elem()
 
-	log.Printf("Input: %#v", data)
-	log.Printf("Receive %#v", val)
+	rData := reflect.ValueOf(data)
 
 	switch val.Kind() {
 	case reflect.Slice:
-		log.Printf("Receive slice")
+		if rData.Type().Kind() != reflect.Slice {
+			return fmt.Errorf("expect slice input, but receive %T", data)
+		}
+		l := rData.Len()
+
+		sliceType := reflect.TypeOf(out).Elem()
+		elType := sliceType.Elem()
+
+		val.Set(reflect.MakeSlice(sliceType, l, l))
+
+		for i := 0; i < l; i++ {
+			temp := reflect.New(elType)
+			if err := i2s(rData.Index(i).Interface(), temp.Interface()); err != nil {
+				return fmt.Errorf("failed to convert slice element: %s", err.Error())
+			}
+			val.Index(i).Set(reflect.Indirect(temp))
+		}
+
 	case reflect.Struct:
+		if rData.Kind() != reflect.Map {
+			return fmt.Errorf("expect map, but receive %v", rData.Kind())
+		}
+		dataMap, _ := data.(map[string]interface{})
 		for i := 0; i < val.NumField(); i++ {
+			valueField := val.Field(i)
 			typeField := val.Type().Field(i)
-			log.Printf("Process field %#v", typeField)
+			inValue := reflect.ValueOf(dataMap[typeField.Name])
+			inValueTypeKind := inValue.Type().Kind()
+			switch typeField.Type.Kind() {
+			case reflect.Int:
+				if inValueTypeKind != reflect.Float64 {
+					return fmt.Errorf("expect float64 on field %s, but receive %T", typeField.Name, inValue)
+				}
+				valueField.SetInt(int64(inValue.Float()))
+			case reflect.String:
+				if inValueTypeKind != reflect.String {
+					return fmt.Errorf("expect string on field %s, but receive %T", typeField.Name, inValue)
+				}
+				valueField.SetString(inValue.String())
+			case reflect.Bool:
+				if inValueTypeKind != reflect.Bool {
+					return fmt.Errorf("expect bool on field %s, but receive %T", typeField.Name, inValue)
+				}
+				valueField.SetBool(inValue.Bool())
+			case reflect.Struct, reflect.Slice:
+				if err := i2s(dataMap[typeField.Name], valueField.Addr().Interface()); err != nil {
+					return fmt.Errorf("failed to fill internal field %s: %s", typeField.Name, err.Error())
+				}
+			}
 		}
 	}
 
